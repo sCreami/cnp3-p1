b074e7b78e8ad6d76d18a54a679cf4f7b2a3a301
Philippe Stormme <myrcellion@gmail.com>
2015-10-16 18:49:36 +0200

    receiver: ajout de la fonction receive_data inclusion de packet.h. receive_data est inspirée par (pompée sur) la fonction perform_transfer de sender.c et permet de récupérer des packets depuis le descripteur de fichiers du socket vers la sortie standard ou le fichier spécifié. receive_data n'a pas été testée et peut contenir des erreurs monumentales.

--- src/receiver.c
+++ src/receiver.c
@@ -7,6 +7,7 @@
 #include "socket.h"
 
 #include "argpars.c" /* void arguments_parser(int argc, char **argv) */
+#include "packet.h" /*packet related functions and structures*/
 
 /* This structure will store the shared parameters of all functions. Its
  * definition can be found in locales.h */
@@ -34,41 +35,63 @@ void print_locales(void)
            locales.passive);
 }
 
-/*int process_received_data(char * buffer, size_t length, int expected_seqnum)
+#define LENGTH 4 + 512 + 4
+
+int receive_data(void)
 {
-    pkt_t *pkt = pkt_new();
-    int decode_status = pkt_decode(buffer, length, pkt);
+    pkt_t *pkt;
+    int ofd, read_size;
+    char buffer[LENGTH];
 
-    if (decode_status != PKT_OK)
-    {
-        //send ack nack
+    ofd = (locales.filename ? open(locales.filename, O_WRONLY) : fileno(stdin));
+
+    if (ofd == -1) {
+        perror("open");
+        return 0;
     }
-    else
+
+    if (locales.verbose)
+        fprintf(stderr, KGRN"[transf]"KNRM" Performing\n");
+
+    while ((read_size = read(locales.sockfd, buffer, sizeof(buffer))))
     {
-        if (expected_seqnum < pkt_get_seqnum);
+        if (read_size < 0) {
+            perror("read");
+            close(ofd);
+            return 0;
+        }
+
+        pkt = pkt_new();
+
+        if (!pkt) {
+            perror("pkt_new");
+            close(ofd);
+            return 0;
+        }
+
+        if (pkt_decode(buffer, (size_t)read_size, pkt) != PKT_OK) {
+            perror("pkt_decode");
+            pkt_del(pkt);
+            close(ofd);
+            return 0;
+        }
+
+        if (write(ofd, pkt->payload, pkt->length) == -1) {
+            perror("write");
+            pkt_del(pkt);
+            close(ofd);
+            return 0;
+        }
+
+        pkt_del(pkt);
     }
 
-    return expected_seqnum;
-}*/
+    close(ofd);
+    close(locales.sockfd);
+    return 1;
+}
 
-/*void receive_loop(int in_fd, int out_fd)
-{
-    pkt_t pkt_buffer[32];
-    uint32_t pkt_buffer_status = 0;
-    int expected_seqnum;
-
-    size_t initial_length = 4 + 512 + 4;
-    sizet_t current_length = initial_length;
-    char * data_buffer[length]
-
-    while (1)
-    {
-        pkt_t * = pkt_new();
-
-
-        pkt_decode()
-    }
-}*/
+#undef LENGTH
 
 int main(int argc, char **argv)
 {
@@ -81,10 +104,8 @@ int main(int argc, char **argv)
 
     ok = connect_socket();
 
-    if (!ok)
-        return EXIT_FAILURE;
-
-    locales.in_fd = locales.sockfd;
+    if (ok)
+        ok = receive_data();
 
     return (ok ? EXIT_SUCCESS : EXIT_FAILURE);
 }
